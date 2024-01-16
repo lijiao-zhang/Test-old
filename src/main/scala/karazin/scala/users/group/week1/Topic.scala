127ce20df78eec46c7efd640a7bf85ef51d12d1a
sisyz <626045049@qq.com>
2024-01-16 18:32:58 +0800

    homework week1

--- src/main/scala/karazin/scala/users/group/week1/Topic.scala
+++ src/main/scala/karazin/scala/users/group/week1/Topic.scala
@@ -2,24 +2,24 @@ package karazin.scala.users.group.week1
 
 import scala.annotation.tailrec
 
-object Topic: 
-  
+object Topic:
+
   // General recursion
-  object Recursion: 
+  object Recursion:
     /*
         For recursive functions explicit 
         result type is required
       */
-    def factorial(n: BigInt): BigInt = 
+    def factorial(n: BigInt): BigInt =
       if n == 0 then 1
-      else n * factorial(n - 1) 
-      
-  /* 
-    Optional construction.
-    It's allowed to use common java-style braces. 
-   */
+      else n * factorial(n - 1)
+
+    /*
+      Optional construction.
+      It's allowed to use common java-style braces.
+     */
   end Recursion
-  
+
   /* 
     Tail recursion
     
@@ -38,12 +38,12 @@ object Topic:
       def factorialReq(n: BigInt, acc: BigInt): BigInt =
         if n == 0 /* Automatically casted to Int */ then acc /* Automatically casted to BigInt */
         else factorialReq(n - 1, acc * n)
-        
+
       factorialReq(n, acc = 1)
-  
-  // Optional construction    
+
+    // Optional construction
   end `Tail Recursion`
-  
+
   object `Higher-Order Functions`:
     /* 
       It's possible to omit type of `v` because 
@@ -51,13 +51,13 @@ object Topic:
      */
     val id: BigInt => BigInt = v => v
     val cube: BigInt => BigInt = v => v * v * v
-    
+
     /*
       `def` implicitly transformed to functional value with
       the type `BigInt => BigInt`
      */
     val factorial: BigInt => BigInt = `Tail Recursion`.factorial
-    
+
     /*
       The function for summing elements in a range [a, b]
       Terrific signature
@@ -66,17 +66,17 @@ object Topic:
         the last `=> BigInt` is for result
      */
     val sum: (BigInt => BigInt) => (BigInt, BigInt) => BigInt =
-      /* 
-        Explicit types for `f`, `a` and `b` could be omited
-        because they are inforced by the explicit type of `sum`.
-        If it's hard to guess the types feel free to use 
-        explicit types. 
-      */ 
-      f => (a, b) => 
+    /*
+      Explicit types for `f`, `a` and `b` could be omited
+      because they are inforced by the explicit type of `sum`.
+      If it's hard to guess the types feel free to use
+      explicit types.
+    */
+      f => (a, b) =>
         // It's allowed to use common java-style `if`s
         if a < 0 || b < 0 || a > b then /* Use then to avoid braces */ 0
         else f(a) + sum(f)(a + 1, b)
-    
+
     /* 
       Optional construction for a long `val` definitions 
       without braces.
@@ -84,22 +84,22 @@ object Topic:
       It's allowed to use common java-style braces.
      */
     end sum
-    
-    val /* or `def` */ sumInts: (BigInt, BigInt) => BigInt = sum(id)   
-    val /* or `def` */ sumCubes: (BigInt, BigInt) => BigInt = sum(cube)   
-    
+
+    val /* or `def` */ sumInts: (BigInt, BigInt) => BigInt = sum(id)
+    val /* or `def` */ sumCubes: (BigInt, BigInt) => BigInt = sum(cube)
+
     /* 
       Compile error
       `def sumFactorials(a: BigInt, b: BigInt): BigInt = sum(id)`
      */
-    
+
     // Works
     def sumFactorials(a: BigInt, b: BigInt): BigInt = sum(factorial)(a, b)
-    
+
     // Also works but the type is undesired
     def anotherSumFactorials(a: BigInt, b: BigInt) /*: (BigInt, BigInt) => BigInt */ = sum(factorial)
-    
-  // Optional construction
+
+    // Optional construction
   end `Higher-Order Functions`
 
 end Topic
