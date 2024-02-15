f701e01e41d8dc27280814d21695d391c05e4b3e
lijiao-zhang <150777482+lijiao-zhang@users.noreply.github.com>
2024-02-15 08:33:17 +0800

    Update HomeworkSpecification.scala

--- src/test/scala/karazin/scala/users/group/week1/homework/HomeworkSpecification.scala
+++ src/test/scala/karazin/scala/users/group/week1/homework/HomeworkSpecification.scala
@@ -21,14 +21,12 @@ object BooleanOperatorsSpecification extends Properties("Boolean Operators"):
   }
 
   property("and") = forAll { (pair: (Boolean, Boolean)) =>
-    val (left, right) = pair
-    
+    val (left, right) = pair 
     and(left, right) == left && right
   }
 
   property("or") = forAll { (pair: (Boolean, Boolean)) =>
-    val (left, right) = pair
-    
+    val (left, right) = pair 
     or(left, right) == left || right
   }   
 
